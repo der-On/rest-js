2a01066364c806e867044d2de57fe133ffb67ead
Ondrej Brinkel <info@anzui.de>
2014-05-04 13:12:23 +0200

    fixes unkown encoding error under node 0.8 when running tests

--- tests/server/lib/rest_request.js
+++ tests/server/lib/rest_request.js
@@ -25,11 +25,29 @@ tests['RestRequest.send() should return a valid RestResponse'] = function(next) 
     assert.equal(response.statusCode, 200);
 
     // data should match our fixture
-    var fixture = fs.readFileSync(path.join(fixturesPath, 'success.json'), { encoding: 'utf8' });
+    var fixture = fs.readFileSync(path.join(fixturesPath, 'success.json'));
     var fixtureJson = require(path.join(fixturesPath, 'success.json'));
     assert.equal(response.data, fixture);
 
     next();
   });
 };
+/*tests['RestRequest.send() should return a Response with an error when called with invalid URL'] = function(next) {
+  var request = new rest.RestRequest('GET', serverUrl + '/should-be-invalid');
+
+  request.send(function(response) {
+    // there should be a response
+    assert.ok(response);
+    assert.ok(response instanceof rest.RestResponse);
+
+    // there should an error
+    assert.ok(response.error);
+
+    // response statusCode should be 404
+    assert.equal(response.statusCode, 404);
+    assert.equal(response.error.message, 'Not found');
+
+    next();
+  });
+};*/
 module.exports = tests;
